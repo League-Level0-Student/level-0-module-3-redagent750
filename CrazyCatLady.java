cc8a6c58364049e7c4525e14957cd3ab97ac4422
redagent750 <37556745+redagent750@users.noreply.github.com>
2018-10-02 13:35:47 -0700

    Add files via upload

---
+++ CrazyCatLady.java
@@ -0,0 +1,52 @@
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+package elseif;
+
+import java.net.URI;
+
+import javax.swing.JOptionPane;
+
+public class CrazyCatLady {
+	public static void main(String[] args) {
+		// 1. Ask the user how many cats they have
+	
+	String	input = JOptionPane.showInputDialog("how many cats do you have?");
+int cats = Integer.parseInt(input);
+
+if(cats>=3) {
+	JOptionPane.showMessageDialog(null, "You are crazy!");
+}
+
+if(cats==0) {
+	playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
+}
+else {
+	playVideo("www.youtube.com/watch?v=ZxMl1SDJ7po");
+}
+
+
+
+
+ 
+		// 2. Convert their answer into an int
+
+		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+
+		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+
+		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+		
+	}
+
+	static void playVideo(String videoURL) {
+		try {
+			URI uri = new URI(videoURL);
+			java.awt.Desktop.getDesktop().browse(uri);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
