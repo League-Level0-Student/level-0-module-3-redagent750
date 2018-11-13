d524321ffe441424df1f5b5f704a8e90624a3b6b
redagent750 <37556745+redagent750@users.noreply.github.com>
2018-11-12 20:40:46 -0800

    Add files via upload

---
+++ oddModulo500.java
@@ -0,0 +1,19 @@
+package modulo;
+
+public class oddModulo500 {
+public static void main(String[]args){
+	System.out.println();
+	
+	for (int i = 1; i < 501; i++) {
+	
+	if(i%2==0) {
+		System.out.print(",");
+	}
+	else {
+		System.out.print(i);
+	}
+	
+	}
+	}
+}
+
